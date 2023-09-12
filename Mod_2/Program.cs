92283ce541408b942b13390a826603fd7032ce11
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 20:50:51 +0600

    Add project files.

---
+++ Mod_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mod_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("X = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 7 * (x * x) - 3 * x + 6;
+            Console.WriteLine("y = " + y);
+        }
+    }
+}
